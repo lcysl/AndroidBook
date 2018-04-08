d3906ac0462689fa3e3961a0e34e98a479cfdea9
lcysl <18628212027@163.com>
2018-04-08 22:04:02 +0800

    创建项目工程，初始化App架构

---
+++ app/src/main/java/com/uestc/lcy/androidbook/AndroidBookApplication.java
@@ -0,0 +1,15 @@
+package com.uestc.lcy.androidbook;
+
+import android.app.Application;
+
+/**
+ * Created by lcy on 2018\4\8 0008.
+ */
+
+public class AndroidBookApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/uestc/lcy/androidbook/modules/MainActivity.java
@@ -0,0 +1,15 @@
+package com.uestc.lcy.androidbook.modules;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uestc.lcy.androidbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
